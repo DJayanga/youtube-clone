25d4234214b8f417bb6516b5ac9f6c11ec07a096
DJayanga <iit17021@std.uwu.ac.lk>
2022-09-09 13:48:08 +0530

    first Commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import SideBar from "./SideBar";
+
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar />
+        <Typography
+          className="copyright"
+          varient="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2022 DJ.
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
